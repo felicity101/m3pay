2598ef3b971572d2064af1f3125a24a81ee9d501
king-hub259 <84419911+king-hub259@users.noreply.github.com>
2025-10-17 06:52:50 -0700

    Update share_controller.dart

--- lib/common/controllers/share_controller.dart
+++ lib/common/controllers/share_controller.dart
@@ -49,7 +49,7 @@ class ShareController extends GetxController implements GetxService{
         final directory = await getApplicationDocumentsDirectory();
         final imageFile = File('${directory.path}/qr.png');
         imageFile.writeAsBytesSync(image!);
-        await GallerySaver.saveImage(imageFile.path,albumName: AppConstants.appName).then((value) => showCustomSnackBarHelper('QR code save to your Gallery',isError: false));
+        await ImageGallerySaver.saveImage(imageFile.path,albumName: AppConstants.appName).then((value) => showCustomSnackBarHelper('QR code save to your Gallery',isError: false));
       }
 
 
