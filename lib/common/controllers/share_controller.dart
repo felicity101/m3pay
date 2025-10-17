1114f460be9746cb41778f5b847fdbc46ffc42c9
king-hub259 <84419911+king-hub259@users.noreply.github.com>
2025-10-17 07:17:42 -0700

    Update share_controller.dart

--- lib/common/controllers/share_controller.dart
+++ lib/common/controllers/share_controller.dart
@@ -10,7 +10,7 @@ import 'package:share_plus/share_plus.dart';
 import 'package:six_cash/common/models/contact_model.dart';
 import 'package:six_cash/helper/route_helper.dart';
 import 'package:get/get.dart';
-import 'package:image_gallery_saver/image_gallery_saver.dart';
+import 'package:photo_manager/photo_manager.dart';
 import 'package:six_cash/util/app_constants.dart';
 import 'package:six_cash/helper/custom_snackbar_helper.dart';
 import 'package:six_cash/features/transaction_money/widgets/share_statement_widget.dart';
@@ -49,9 +49,7 @@ class ShareController extends GetxController implements GetxService{
         final directory = await getApplicationDocumentsDirectory();
         final imageFile = File('${directory.path}/qr.png');
         imageFile.writeAsBytesSync(image!);
-       final bytes = await imageFile.readAsBytes();
-await ImageGallerySaver.saveImage(bytes, name: AppConstants.appName);
-
+        await PhotoManager.saveImage(imageFile.path,albumName: AppConstants.appName).then((value) => showCustomSnackBarHelper('QR code save to your Gallery',isError: false));
       }
 
 
